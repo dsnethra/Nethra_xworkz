e08d0f3f08c3ab7ae4d87220c1db4252c63f04e9
dsnethra <nethravathids2001@gmail.com>
2023-06-28 23:15:08 +0530

    day 28 task

---
+++ God.java
@@ -0,0 +1,296 @@
+//new method (new constructor)  new is keyword, and constructor is class name 
+// class is nothing but it has blue print of the object
+// new= it is a key word, it will help to crate the copy of the object  
+//constructor 
+// constructor is nothing but class name, every class has constructor
+// syntax of specialmethod     non acess specifie new classname() throws class name{    in general ex: new God()
+
+class God {
+	God()
+	{
+	System.out.println("god running");
+	}
+}
+
+
+
+
+class GodRunner{
+	public static void main(String[] args){
+	System.out.println("running main ");
+new God();     // 
+
+God name1= new God();        // instantiation
+God name2 = new God();
+God name3 = new God();
+God name4 = new God();
+God name5 = new God();
+God name6 = new God();
+God name7 = new God();
+God name8 = new God();
+God name9 = new God();
+God name10 = new God();
+God name11 = new God();
+	}
+}
+	
+	
+	
+//2nd one	
+	
+class Ball {
+	Ball()
+	{
+		System.out.println("running ball");
+	}
+}
+
+
+class BallRunner {
+	public static void main(String[] args) {
+		System.out.println("running main ballrunner");
+		new Ball();
+		Ball ball1=new Ball();
+		Ball ball2=new Ball();
+		Ball ball3=new Ball();
+		Ball ball4=new Ball();
+		Ball ball5=new Ball();
+		Ball ball6=new Ball();
+		Ball ball7=new Ball();
+		Ball ball8=new Ball();
+		Ball ball9=new Ball();
+		Ball ball10=new Ball();
+		Ball ball11=new Ball();
+		
+	}
+}
+
+
+
+//  3rd one
+
+class Phone {
+	Phone()
+	{
+		System.out.println("running Phone");
+	}
+}
+
+
+
+class PhoneRunner {
+	public static void main(String[] args){
+		System.out.println("running main");
+		new Phone();
+		Phone phone1= new Phone();
+		Phone phone2= new Phone();
+		Phone phone3= new Phone();
+		Phone phone4= new Phone();
+		Phone phone5= new Phone();
+		Phone phone6= new Phone();
+		Phone phone7= new Phone();
+		Phone phone8= new Phone();
+		Phone phone9= new Phone();
+		Phone phone10= new Phone();
+		Phone phone11= new Phone();
+		
+	}
+}
+
+
+
+//4th one
+
+
+class Charger {
+	Charger ()
+	{
+		System.out.println("charger is important to charge");
+	}
+}
+
+
+class ChargerRunner {
+	public static void main (String[] args){
+		System.out.println("running ChargerRunner");
+		new Charger();
+		Charger name1= new Charger();
+		Charger name2= new Charger();
+		Charger name3= new Charger();
+		Charger name4= new Charger();
+		Charger name5= new Charger();
+		Charger name6= new Charger();
+		Charger name7= new Charger();
+		Charger name8= new Charger();
+		Charger name9= new Charger();
+		Charger name10= new Charger();
+		Charger name11= new Charger();
+	}
+}
+
+
+
+//5th one 
+
+class Sky {
+	Sky()
+	{
+		System.out.println("Sky is in blue colour");
+	}
+}
+
+
+class SkyRunner{
+	public static void main(String[] args){
+		System.out.println("runnig Sj=kyrunner");
+		new Sky();
+		Sky sky1=new Sky();
+		Sky sky2=new Sky();
+		Sky sky3=new Sky();
+		Sky sky4=new Sky();
+		Sky sky5=new Sky();
+		Sky sky6=new Sky();
+		Sky sky7=new Sky();
+		Sky sky8=new Sky();
+		Sky sky9=new Sky();
+		Sky sky10=new Sky();
+		Sky sky11=new Sky();
+	}
+}
+
+// 6th one
+
+class Money {
+	Money(){
+		System.out.println("money kills 99% of problem");
+	}
+}
+
+
+class MoneyRunner {
+	public static void main(String[] args){
+		System.out.println("Running Moneyrunner");
+		Money type1= new Money();
+		Money type2= new Money();
+		Money type3= new Money();
+		Money type4= new Money();
+		Money type5= new Money();
+		Money type6= new Money();
+		Money type7= new Money();
+		Money type8= new Money();
+		Money type9= new Money();
+		Money type10= new Money();
+		Money type11= new Money();
+	}
+}
+
+
+
+//7th one 
+
+class Floor {
+	Floor() {
+		System.out.println("we are in ground floor");
+	}
+}
+
+class FloorRunner{
+	public static void main(String[] args){
+		System.out.println("running floorrunner");
+		Floor floor1=new Floor();
+		Floor floor2=new Floor();
+		Floor floor3=new Floor();
+		Floor floor4=new Floor();
+		Floor floor5=new Floor();
+		Floor floor6=new Floor();
+		Floor floor7=new Floor();
+		Floor floor8=new Floor();
+		Floor floor9=new Floor();
+		Floor floor10=new Floor();
+		Floor floor11=new Floor();
+	}
+}
+
+
+// 8th one
+
+class Pg {
+	Pg()
+	{
+		System.out.println("pg running");
+	}
+}
+
+class PgRunner{
+	public static void main(String[] args){
+		System.out.println("running main");
+		new Pg();
+		Pg pg1= new Pg();
+		Pg pg2= new Pg();
+		Pg pg3= new Pg();
+		Pg pg4= new Pg();
+		Pg pg5= new Pg();
+		Pg pg6= new Pg();
+		Pg pg7= new Pg();
+		Pg pg8= new Pg();
+		Pg pg9= new Pg();
+		Pg pg10= new Pg();
+		Pg pg11= new Pg();
+	}
+}
+
+
+// 9th one 
+
+class Jewellary {
+	Jewellary()
+	{
+		System.out.println("cost of Jewellary is high");
+	}
+}
+
+class JewellaryRun {
+	public static void main(String[] args){
+		new Jewellary();
+		Jewellary name1=new Jewellary();
+		Jewellary name2=new Jewellary();
+		Jewellary name3=new Jewellary();
+		Jewellary name4=new Jewellary();
+		Jewellary name5=new Jewellary();
+		Jewellary name6=new Jewellary();
+		Jewellary name7=new Jewellary();
+		Jewellary name8=new Jewellary();
+		Jewellary name9=new Jewellary();
+		Jewellary name10=new Jewellary();
+		Jewellary name11=new Jewellary();
+	}
+}
+		
+		
+	// 10th one 
+	
+	class Monkey {
+		Monkey()
+		{
+			System.out.println("running monkey");
+		}
+	}
+	
+	
+	class MonkeyRunner {
+		public static void main(String[] args){
+			new Monkey();
+			Monkey name1= new Monkey();
+			Monkey name2= new Monkey();
+			Monkey name3= new Monkey();
+			Monkey name4= new Monkey();
+			Monkey name5= new Monkey();
+			Monkey name6= new Monkey();
+			Monkey name7= new Monkey();
+			Monkey name8= new Monkey();
+			Monkey name9= new Monkey();
+			Monkey name10= new Monkey();
+			Monkey name11= new Monkey();
+			
+		}
+	}
